a906cc020381d3e4210cf917922512cadd0ce38b
RitikOjha26 <ojha.ritik99680@gmail.com>
2023-10-26 19:48:59 +0530

    feat: Styling improvements

--- src/component/Banners/BottomBanner2.js
+++ src/component/Banners/BottomBanner2.js
@@ -9,26 +9,26 @@ const BottomBanner2 = () => {
         <div class="w-screen h-[70vh] flex  justify-center items-center bg-white mt-24">
 
             {/* <!-- Parent div with 100vw width and margin bg-[#0052FE]--> */}
-            <div class="w-[85vw] h-[45vh]  rounded-3xl bg-green-600   z-10 flex flex-row ">
-                {/* <div className="flex  h-auto "> */}
+            <div class="w-[85vw] h-[45vh]  rounded-3xl bg-[#0052FE]   z-10 flex flex-row ">
+                <div className="flex m-auto p-[2rem] items-center justify-center gap-[3rem] w-auto h-auto ">
 
-                <img src={Banner1} alt="Banner2" className='h-[200px] w-[200px] relative   ' />
-                <div className="  w-[50%]  pt-8">
+                    <img src={Banner1} alt="Banner2" className='h-[200px] w-[200px] relative   ' />
+                    <div className="  w-auto flex flex-col justify-between content-center    ">
 
-                    <h1 className=' text-[20px] font-bold pb-4 '>Stay up to date with latest crypto news and events. Subscribe to our newsletter</h1>
-                    <div className="flex flex-row">
-                        <input
-                            type="email"
-                            className="block h-[7vh] w-auto px-4  mr-2 rounded leading-tight bg-gray-200 text-gray-700 border focus:outline-none focus:bg-white focus:border-gray-500"
-                            placeholder="Enter your Email Address"
+                        <h1 className=' text-[1.4rem] flex  font-bold text-white pb-4 '>Stay up to date with latest crypto news and events. Subscribe to our newsletter</h1>
+                        <div className="flex flex-row">
+                            <input
+                                type="email"
+                                className="block h-[7vh] w-[16rem] px-4  mr-2 rounded leading-tight bg-gray-200 text-gray-700 border focus:outline-none focus:bg-white focus:border-gray-500"
+                                placeholder="Enter your Email Address"
 
-                        />
-                        <button className='bg-[#FFB800] text-black font-medium w-auto h-[7vh] rounded-md'>
-                            <span className=' text-xl'>Suscribe</span>
-                            <FontAwesomeIcon icon={faArrowRight} className='ml-auto' />
-                        </button>
+                            />
+                            <button className='bg-[#FFB800] text-black font-medium  h-[7vh] w-[12rem] rounded-md'>
+                                <span className=' text-xl'>Suscribe</span>
+                                <FontAwesomeIcon icon={faArrowRight} className='ml-auto' />
+                            </button>
+                        </div>
                     </div>
-                {/* </div> */}
                 </div>
 
             </div>
